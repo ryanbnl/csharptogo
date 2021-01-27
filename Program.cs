02dbef9e01de1299ac2b86fb4163b18b8f854578
Ryan Barrett <ryan@seldonplan.com>
2021-01-27 11:07:54 +0100

    Added extended interfaces

--- Program.cs
+++ Program.cs
@@ -7,31 +7,63 @@ namespace CSharpClient
     // extern char* HelloWorld(char* hello);
     class Program
     {
-        [DllImport(
-            "helloworld.dll",
-            CharSet = CharSet.Unicode,
-            CallingConvention = CallingConvention.Cdecl)]
-        //[return: MarshalAs(UnmanagedType.LPStr)]
+        [DllImport("helloworld.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
         static extern IntPtr HelloWorld();
 
-        [DllImport(
-            "helloworld.dll",
-            CharSet = CharSet.Unicode,
-            CallingConvention = CallingConvention.Cdecl)]
-        //[return: MarshalAs(UnmanagedType.LPStr)]
+        [DllImport("helloworld.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
         static extern IntPtr HelloGalaxy();
 
+        [DllImport("helloworld.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        static extern IntPtr GenerateIssuerNonceB64();
+
+
+        [DllImport("helloworld.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        static extern IntPtr Issue(string issuerPkXml, string issuerSkXml, string issuerNonceB64, string commitmentsJson);
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Calling Go.HelloWorld");
-            var x = Marshal.PtrToStringAnsi(HelloWorld());
-            Console.WriteLine("Answer " + x);
-
+            TestHelloWorld();
             Console.WriteLine("");
+            TestHelloGalaxy();
+            Console.WriteLine("");
+            TestGetIssuerNonce();
+            Console.WriteLine("");
+            TestIssue();
+        }
 
+        private static void TestHelloWorld()
+        {
+            Console.WriteLine("Calling Go.HelloWorld");
+            var result = Marshal.PtrToStringAnsi(HelloWorld());
+            Console.WriteLine("Answer " + result);
+        }
+
+        private static void TestHelloGalaxy()
+        {
             Console.WriteLine("Calling Go.HelloGalaxy");
-            var y = Marshal.PtrToStringAnsi(HelloGalaxy());
-            Console.WriteLine("Answer " + y);
+            var result = Marshal.PtrToStringAnsi(HelloGalaxy());
+            Console.WriteLine("Answer " + result);
+        }
+
+        private static void TestGetIssuerNonce()
+        {
+            Console.WriteLine("Calling Go.HelloGalaxy");
+            var result = Marshal.PtrToStringAnsi(GenerateIssuerNonceB64());
+            Console.WriteLine("Answer " + result);
+        }
+
+        private static void TestIssue()
+        {
+            Console.WriteLine("Calling Go.HelloGalaxy");
+
+            var issuerPkXml = "PkXml";
+            var issuerSkXml = "SkXml";
+            var issuerNonceB64 = "NONCE";
+            var commitmentsJson = "{ commitments: true }";
+
+            var result = Marshal.PtrToStringAnsi(Issue(issuerPkXml, issuerSkXml, issuerNonceB64, commitmentsJson));
+
+            Console.WriteLine("Answer " + result);
         }
     }
 }
--- main.go
+++ main.go
@@ -19,3 +19,18 @@ func HelloGalaxy() *C.char {
 	fmt.Println(msg)
 	return C.CString(msg)
 }
+
+//export GenerateIssuerNonceB64
+func GenerateIssuerNonceB64() *C.char {
+	issuerNonceB64 := "Do the twist and shake"
+	return C.CString(issuerNonceB64)
+}
+
+//export Issue
+func Issue(issuerPkXml, issuerSkXml, issuerNonceB64, commitmentsJson *C.char) *C.char {
+	fmt.Println(issuerPkXml)
+	fmt.Println(issuerSkXml)
+	fmt.Println(issuerNonceB64)
+	fmt.Println(commitmentsJson)
+	return C.CString("This darkness, it's smothering me")
+}
